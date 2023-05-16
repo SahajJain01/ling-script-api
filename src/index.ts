e6e6b051c86597e32a08f82321baf109e46ed87b
Sahaj Jain <jainsahaj@gmail.com>
2023-05-16 21:20:06 +0530

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -182,7 +182,5 @@ app.get("/prompt/:langId/:id", async (req, res) => {
 // })
 
 const server = app.listen(3000, () =>
-  console.log(`
-🚀 Server ready at: http://localhost:3000
-⭐️ See sample requests: http://pris.ly/e/ts/rest-express#3-using-the-rest-api`)
+  console.log(`Server ready at: http://localhost:3000`)
 );
